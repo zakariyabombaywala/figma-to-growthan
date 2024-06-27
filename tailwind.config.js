ed8ffe73c755db2450217fdb58fd8e6a2799ea45
zakariyabombywala <zakariyabombaywala@gmail.com>
2024-06-27 15:38:30 +0530

    m: testimonial section complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,22 @@ module.exports = {
       colors: {
         blue: {
           DEFAULT: "#162466",
+          100: "#F5F7FF",
           200: "#E9F7FF",
           300: "#E9F0FF",
           900: "#181B31",
         },
         red: {
           DEFAULT: "#FF3B33",
+          100: "#FFF8F8",
           400: "#FFDBD4",
         },
         yellow: {
+          DEFAULT: "#FFB545",
           300: "#FFF3CA",
+        },
+        gray: {
+          DEFAULT: "#555555",
         },
       },
     },
